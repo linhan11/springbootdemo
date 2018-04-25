74996a40256b42765d9c0d5d31beaa1150af46e6
西野 泰暢 <Yasunobu_Nishino@hulft.com>
2018-04-25 11:36:14 +0900

    ログイン画面の修正とログアウトボタン追加

--- src/main/java/jp/co/saison/tvc/springbootdemo/app/WebSecurityConfig.java
+++ src/main/java/jp/co/saison/tvc/springbootdemo/app/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
+	            // ログアウトがパス(GET)の場合設定する（CSRF対応）
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	            // ログアウトがPOSTの場合設定する
+	            //.logoutUrl("/logout")
+	            // ログアウト後の遷移先
+	            .logoutSuccessUrl("/login")
+	            // セッションを破棄する
+	            .invalidateHttpSession(true)
+	            // ログアウト時に削除するクッキー名
+	            .deleteCookies("JSESSIONID")
                 .permitAll();
     }
 /*
