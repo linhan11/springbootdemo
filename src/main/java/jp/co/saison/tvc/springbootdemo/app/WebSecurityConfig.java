4020806af30508b554bb108e7cf6746c7256ae53
西野 泰暢 <Yasunobu_Nishino@hulft.com>
2018-04-26 18:02:38 +0900

    ログイン後の画面の配置作成

--- src/main/java/jp/co/saison/tvc/springbootdemo/app/WebSecurityConfig.java
+++ src/main/java/jp/co/saison/tvc/springbootdemo/app/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/users/**").permitAll()
+                .antMatchers("/css/**", "/js/**", "/users/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
