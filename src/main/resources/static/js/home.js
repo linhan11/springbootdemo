9641960030fe66a5ac9ef5184e077f3ec56292ab
Takeshi_Morita <Takeshi_Morita@hulft.com>
2018-05-10 14:36:25 +0900

    対戦申込みと返却途中

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,9 +1,5 @@
 var game = {}
 
-game.id = "";
-game.turn = "O";
-game.gridid = "";
-game.map = [ [ "", "", "" ], [ "", "", "" ], [ "", "", "" ] ];
 
 function send_message() {
 	console.log(game);
@@ -12,7 +8,7 @@ function send_message() {
 /*
  * 配列へ変換
  */
-function convert_map(game) {
+function convert_map() {
 	var tmp;
 	var i;
 	var x = 0;
@@ -28,22 +24,28 @@ function convert_map(game) {
 	}
 }
 
+
 /*
- * 判定
+ * 判定 x x x
+ *
+ * y y y
+ *
  * x x x
- *
- * y
- * y
- * y
- *
- * x
- *   x
- *     x
  */
-function check_map(game) {
+function check_map() {
 }
 
-function set_piece(game) {
+function set_piece() {
+	if (game.status != "start") {
+		console.log("status   : " + game.status);
+		return;
+	}
+	if (game.piece != game.turn) {
+		$("#turnstatus").text(game.turn)
+	} else {
+
+	}
+
 	var gridid = game.gridid;
 	var tmp = $("#" + gridid).text();
 	if (tmp != "") {
@@ -66,8 +68,7 @@ function set_piece(game) {
 	} else {
 		game.turn = "X";
 	}
-
-	$("#turnstatus").text(game.turn)
+	utl_turn_message();
 }
 
 /*
@@ -78,14 +79,23 @@ function get_loginuser() {
 }
 
 $(document).ready(function() {
+	game.status = "";
+	game.userid = "";
+	game.id = "";
+	game.targetid = "";
+	game.turn = "O";
+	game.gridid = "";
+	game.piece = "";
+	game.map = [ [ "", "", "" ], [ "", "", "" ], [ "", "", "" ] ];
+
 	for (var i = 1; i <= 9; i++) {
 		$("#grid_" + i).click(function() {
+
 			console.log("name   : " + $("#name").val());
 			console.log("div id : " + this.id);
-			game.id = $("#name").val();
 			game.gridid = this.id;
 
-			set_piece(game);
+			set_piece();
 		})
 	}
 
@@ -93,32 +103,38 @@ $(document).ready(function() {
 	// ゲーム用websocket
 	var user = get_loginuser();
 	var uri = "ws://" + location.host + "/websocket/game" + '?' + user;
+	game.userid = user;
 
 	ws = new WebSocket(uri);
 	ws.onopen = function() {
 		var loginData = new Object();
 		loginData.proto = "login";
 		loginData.user = user;
+		game.status = "login"
 		ws.send(JSON.stringify(loginData));
 	};
 	ws.onmessage = function(message) {
 		try {
 			wsRes = JSON.parse(message.data);
+
 			console.log("onmessage()");
-			console.log("proto : " + wsRes.proto);
+			console.log("message : " + message);
+			console.log("  proto : " + wsRes.proto);
 
 			if (wsRes.proto == "login_list") {
 				//alert(wsRes);
+				recv_login_list(wsRes);
 				makeUserTable(wsRes);
 			} else if (wsRes.proto == "matchWithReq") {
 				// 対戦の申し込み
 				console.log(wsRes);
-				recv_play_approach(wsRes);
+				recv_play_matchWithReq(wsRes);
 			} else if (wsRes.proto == "matchWithRep") {
 				// 対戦の申し込み結果
 				console.log(wsRes);
-			} else {
-				alert(wsRes.proto);
+				recv_play_matchWithRep(wsRes);
+			} else if (wsRes.proto == "matchStart") {
+				recv_play_matchStart(wsRes);
 			}
 		} catch (e) {
 			alert(e);
@@ -132,29 +148,8 @@ $(document).ready(function() {
 
 })
 
-/*
- * // 対戦の申し込み {"proto":"matchWithReq","targetID":"targetSeesionID"}
- */
-function send_play_approach(sessionid) {
-	console.log("send_play_approach()");
-	var data = {};
-	data.proto = "matchWithReq";
-	data.targetID = sessionid;
-	console.log(data);
-	ws.send(JSON.stringify(data));
-
-}
-
-/*
- * 対戦の申し込み結果 {"proto":"matchWithRep","targetID":"targetSeesionID","status":"OK or NG"}
- */
-function recv_play_approach(data) {
-	$('#turnstatus').text(data.targetID);
-}
-
-
 function clearUsertable(name) {
-	$("#userlist").empty();
+	$("#userlist").children().remove();
 }
 
 /*
@@ -163,6 +158,8 @@ function clearUsertable(name) {
  *  一覧を作成し、対戦ボタンにセッションIDを保持
  */
 function makeUserTable(data) {
+	console.log("makeUserTable()");
+
 	clearUsertable("userlist");
 	for (var i = 0; i < data.login_list.length; i++) {
 		$("#UserTable")
@@ -177,15 +174,120 @@ function makeUserTable(data) {
 								+ data.login_list[i].id
 								+ '" class="game-play btn btn-danger">対戦</button></td></tr>')
 		var button = document.getElementById(data.login_list[i].id);
-		button.addEventListener("click",myfunc);
+		button.addEventListener("click", myfunc);
 	}
 }
 
 function myfunc(event) {
 	console.log("session id : " + this.id);
 	var target = $(event.target);
+
 	target.text("依頼");
-	send_play_approach(this.id);
+	game.button = target;
+	send_play_matchWithReq(this.id);
+}
+
+/*
+ * ログイン時
+ *  {"proto":"login_list","id":"1", "login_list":[]}
+ */
+function recv_login_list(data) {
+	console.log("recv_login_list()");
+	game.id = data.id;
+	makeUserTable(data);
+}
+
+/*
+ * // 対戦の申し込み {"proto":"matchWithReq","targetID":"targetSeesionID"}
+ */
+function send_play_matchWithReq(sessionid) {
+	console.log("send_play_matchWithReq()");
+
+	var data = {};
+
+	data.proto = "matchWithReq";
+	game.targetid =
+		data.targetID = sessionid;
+	data.user = game.userid;
+	data.status = "";
+
+	console.log(data);
+	ws.send(JSON.stringify(data));
+
+}
+
+/*
+ * 対戦の申し込み結果 {"proto":"matchWithRep","targetID":"targetSeesionID","status":"OK or NG"}
+ */
+
+function recv_play_matchWithReq(data) {
+
+	console.log("recv_play_matchWithReq()");
+
+	var data = {};
+
+	data.targetID = game.id;
+	data.proto = "matchWithRep";
+	if (game.status == "login") {
+		$("#show_dialog").dialog({
+			modal : true,
+			title : data.targetID + "と対戦",
+			buttons : {
+				"OK" : function() {
+					$(this).dialog("close");
+					data.status = "OK";
+					game.status = "start";
+				},
+				"キャンセル" : function() {
+					$(this).dialog("close");
+					data.status = "NG";
+				}
+			}
+		});
+
+	} else {
+		data.status = "NG";
+	}
+	data.user = game.userid;
+
+	console.log(data);
+
+	ws.send(JSON.stringify(data));
+}
+
+function recv_play_matchWithRep(data) {
+	console.log("recv_play_matchWithRep");
+	game.status = "login";
+	console.log(" NG だった?");
+	console.log(" status : " + data.status);
+
+}
+
+function utl_set_status(msg) {
+	$("#turnstatus").text(msg);
+}
+
+function utl_turn_message() {
+	if (game.piece == game.turn) {
+		utl_set_status("あなたの番です");
+	} else {
+		utl_set_status("あいての番です");
+	}
+}
+
+function recv_play_matchStart(data) {
+	console.log("recv_play_matchStart");
+	console.log("status : " + data.status);
+
+	game.status = "start";
+	game.turn = "O";
+	//game.button.text("対戦中");
+	if (data.status == "First") {
+		game.piece = "O";
+	} else {
+		game.piece = "X";
+	}
+	utl_turn_message();
 }
 
 
@@ -233,6 +335,7 @@ $(function() {
 				webSocket.send(get_loginuser() + " : "
 						+ $('#message').val());
 			});
+
 });
 
 
